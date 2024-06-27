278ed1d4837fc057251c0bc17121fd2e3065f6df
Gabriel Rodríguez Castellini <gabriel.rodriguezcastellini@outlook.com>
2024-06-27 19:59:48 -0300

    Handling UI Components

--- playwright.config.js
+++ playwright.config.js
@@ -10,6 +10,6 @@ export default defineConfig({
   reporter: "html",
   use: {
     browserName: "chromium",
-    headless: true,
+    headless: false,
   },
 });
--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
-test.only("Browser Context Playwright test", async ({ browser }) => {
+test("Browser Context Playwright test", async ({ browser }) => {
   const context = await browser.newContext();
   const page = await context.newPage();
   const userName = page.locator("#username");
@@ -25,4 +25,38 @@ test("Page Playwright test", async ({ page }) => {
   await page.goto("https://www.google.com");
   console.log(await page.title());
   await expect(page).toHaveTitle("Google");
+});
+
+test("UI Controls", async ({ page }) => {
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  const documentLink = page.locator("[href*='documents-request']");
+  const dropdown = page.locator("select.form-control");
+  await dropdown.selectOption("consult");
+  await page.locator(".radiotextsty").last().click();
+  await page.locator("#okayBtn").click();
+  console.log(await page.locator(".radiotextsty").last().isChecked());
+  await expect(page.locator(".radiotextsty").last()).toBeChecked();
+  await page.locator("#terms").click();
+  await expect(page.locator("#terms")).toBeChecked();
+  await page.locator("#terms").uncheck();
+  expect(await page.locator("#terms").isChecked()).toBeFalsy();
+  await expect(documentLink).toHaveAttribute("class", "blinkingText");
+});
+
+test.only("@Child windows handle", async ({ browser }) => {
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  const userName = page.locator("#username");
+  await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+  const documentLink = page.locator("[href*='documents-request']");
+  const [newPage] = await Promise.all([
+    context.waitForEvent("page"),
+    documentLink.click(),
+  ]);
+  const text = await newPage.locator(".red").textContent();
+  const arrayText = text.split("@");
+  const domain = arrayText[1].split(" ")[0];
+  console.log(domain);
+  await page.locator("#username").fill(domain);
+  await page.pause();
 });
